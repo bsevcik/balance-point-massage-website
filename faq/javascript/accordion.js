d05fb42ed0c6fbb33e1c7ae4d58d802466fbe8d8
Ben Sevcik <43259346+bsevcik@users.noreply.github.com>
2019-01-23 10:29:21 -0800

    made last few accordion buttons scroll to bottom when clicked

--- faq/javascript/accordion.js
+++ faq/javascript/accordion.js
@@ -2,10 +2,14 @@ $(function() {
 	$('.ss_button').on('click',function() {
 		if ($(this).next('.ss_content').is(":visible")) {
 			$(this).next('.ss_content').slideToggle('fast');
+		} else if ($(this).hasClass("scrollToBottom")){ 
+			$('.ss_content').slideUp('fast');
+			$(this).next('.ss_content').slideDown('fast');
+			$('html, body').animate({scrollTop:$(document).height()}, 'slow');
+			return false;
 		} else {
 			$('.ss_content').slideUp('fast');
 			$(this).next('.ss_content').slideDown('fast');
-
 		}
 			
 	});
